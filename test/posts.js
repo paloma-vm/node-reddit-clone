53c88df34d3dc8660fa501e586c772c9c12f0c52
paloma-vm <paloma.valdez-marsh@students.dominican.edu>
2023-05-12 16:29:35 -0600

    fixed posts test

--- test/posts.js
+++ test/posts.js
@@ -1,7 +1,7 @@
 // test/posts.js
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const { describe, it, after} = require('mocha');
+const { describe, it, before, after} = require('mocha'); // remember to require before
 chai.use(chaiHttp);
 const app = require('../server');
 const agent = chai.request.agent(app);
@@ -9,6 +9,7 @@ const agent = chai.request.agent(app);
 // Import the Post model from our models folder so we
 // we can use it in our tests.
 const Post = require('../models/post');
+const User = require('../models/user');
 
 const should = chai.should();
 
@@ -17,8 +18,29 @@ describe('Posts', () => {
   const newPost = {
     title: 'post title',
     url: 'https://www.google.com',
-    summary: 'post summary'
+    summary: 'post summary',
+    subreddit: 'Test Posts' // added this myself, subreddit was missing and the test failed, it passes now
   };
+  // User that we'll use for testing purposes
+  const user = {
+    username: 'poststest',
+    password: 'testposts',
+  };
+
+  before(function (done) {
+    agent
+      .post('/sign-up')
+      .set('content-type', 'application/x-www-form-urlencoded')
+      .send(user)
+      .then(function (res) {
+        done();
+      })
+      .catch(function (err) {
+        done(err);
+      });
+  });
+
+
   it('should create with valid attributes at POST /posts/new', (done) => {
     // Checks how many posts there are now
     Post.estimatedDocumentCount()
@@ -52,8 +74,25 @@ describe('Posts', () => {
         });
     });
 
-    after(() => {
-    Post.findOneAndDelete(newPost);
+    after(function (done) {
+      Post.findOneAndDelete(newPost)
+      .then(function () {
+        agent.close();
+    
+        User
+          .findOneAndDelete({
+            username: user.username,
+          })
+          .then(function () {
+            done();
+          })
+          .catch(function (err) {
+            done(err);
+          });
+      })
+      .catch(function (err) {
+        done(err);
+      });
     });
 // Make a request to create another
 // Check that the database has one more post in it
