f0fe5b1e50fc4fcddd94f9121768d810f6f571cd
paloma-vm <paloma.valdez-marsh@students.dominican.edu>
2023-05-13 11:47:39 -0600

    attempted nested comments
    not nesting, problem with bootstrap?

--- controllers/replies.js
+++ controllers/replies.js
@@ -4,46 +4,37 @@ const Comment = require('../models/comment');
 
 module.exports = (app) => {
   // NEW REPLY
-  app.get('/posts/:postId/comments/:commentId/replies/new', (req, res) => {
-    const currentUser = req.user;
-    let post;
-    Post.findById(req.params.postId).lean()
-      .then((p) => {
-        post = p;
-        return Comment.findById(req.params.commentId).lean();
-      })
-      .then((comment) => {
-        res.render('replies-new', { post, comment, currentUser });
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
+  app.get('/posts/:postId/comments/:commentId/replies/new', async (req, res) => {
+    try {
+      const currentUser = req.user;
+      const post = await Post.findById(req.params.postId).lean();
+      const comment = await Comment.findById(req.params.commentId).lean();
+      res.render('replies-new', { post, comment, currentUser });
+    } catch (err) {
+      console.log(err.message);
+    }
   });
 
   // CREATE REPLY
-  app.post('/posts/:postId/comments/:commentId/replies', (req, res) => {
-    // TURN REPLY INTO A COMMENT OBJECT
-    const reply = new Comment(req.body);
-    reply.author = req.user._id;
-    // LOOKUP THE PARENT POST
-    Post.findById(req.params.postId)
-      .then((post) => {
-        // FIND THE CHILD COMMENT
-        Promise.all([
-          reply.save(),
-          Comment.findById(req.params.commentId),
-        ])
-          .then(([reply, comment]) => {
-            // ADD THE REPLY
-            comment.comments.unshift(reply._id);
-            return Promise.all([
-              comment.save(),
-            ]);
-          })
-          .then(() => res.redirect(`/posts/${req.params.postId}`))
-          .catch(console.error);
-        // SAVE THE CHANGE TO THE PARENT DOCUMENT
-        return post.save();
-      });
+  app.post('/posts/:postId/comments/:commentId/replies', async (req, res) => {
+    try {
+      // TURN REPLY INTO A COMMENT OBJECT
+      const reply = new Comment(req.body);
+      reply.author = req.user._id;
+      // LOOKUP THE PARENT POST
+      const post = await Post.findById(req.params.postId);
+      // FIND THE CHILD COMMENT
+      const [savedReply, comment] = await Promise.all([
+        reply.save(),
+        Comment.findById(req.params.commentId),
+      ]);
+      // ADD THE REPLY
+      comment.comments.unshift(savedReply._id);
+      await comment.save();
+      await post.save();
+      res.redirect(`/posts/${req.params.postId}`);
+    } catch (err) {
+      console.error(err);
+    }
   });
 };
